c2f13b6745d8f9e23677b7b9aeb61f344710380c
Trong-Hieu Nguyen <work@hieunguyen.de>
2025-10-14 21:59:17 +0200

    update packages to install R package

--- install_R_packages2.R
+++ install_R_packages2.R
@@ -1,10 +1,12 @@
-new.pkgs <- c("argparse", "shinyjs", "shinyalert", "DT", "dplyr", "pillar", "this.path", "readxl", "writexl")
+new.pkgs <- c("tidyverse", "argparse", "shinyjs", "shinyalert", "DT", "dplyr", "pillar", "this.path", "readxl", "writexl")
 for (pkg in new.pkgs){
   if (pkg %in% installed.packages() == FALSE){
     install.packages(pkg)
   }  
 }
-new.bioc.pkgs <- c("pathview", "clusterProfiler",  "DOSE", "org.Hs.eg.db", "org.Mm.eg.db", "scRepertoire")
+new.bioc.pkgs <- c("pathview", "clusterProfiler",  
+"DOSE", "org.Hs.eg.db", "org.Mm.eg.db", "scRepertoire",
+"biomaRt", "msigdbr")
 for (pkg in new.bioc.pkgs){
   if (pkg %in% installed.packages() == FALSE){
     BiocManager::install(pkg, update = FALSE)
@@ -14,3 +16,4 @@ for (pkg in new.bioc.pkgs){
 if ("DoubletFinder" %in% installed.packages() == FALSE){
   remotes::install_github('chris-mcginnis-ucsf/DoubletFinder', force = TRUE)
 }
+BiocManager::install('glmGamPoi', update = FALSE)
