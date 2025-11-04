3761de307a50a75a37ecc9b4c0622da5aa283c1d
Trong-Hieu Nguyen <work@hieunguyen.de>
2025-11-04 13:10:29 +0100

    update

---
+++ run_docker_container_deploy_CRC1382.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+port=$1;
+name=$2;
+docker_name=$3;
+
+sudo docker run -it \
+    -v /Users/hieunguyen:/home/hieunguyen \
+    -v /Volumes:/media -p ${port}:8787 -p 4141:4141 \
+    -e PASSWORD=genov4 --name ${name} \
+    ${docker_name}:latest
+
+# bash run_docker_container_deploy_CRC1382.sh 8181 rstudio1 tronghieunguyen/single_cell_pipeline 
